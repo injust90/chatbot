979e0de705e59babfbff60dc1acadcfc4dde625c
KawaBaud <187086553+KawaBaud@users.noreply.github.com>
2025-10-19 04:27:28 +0300

    this was a bit painful. Godot, ALLOW INVERTING SLIDER KNOB YOU F*CKS

--- Scenes/volume_slider.gd
+++ Scenes/volume_slider.gd
@@ -1,21 +1,18 @@
 extends HSlider
 
-@export
-var bus_name: String
+const MAX_VOL_DB = 100.0
 
+@export var bus_name: String
 var bus_index: int
 
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_volume_changer)
-	
-	#value = db_to_linear(
-		#AudioServer.get_bus_volume_db(bus_index)
-	#)
-	
-func _volume_changer(value: float) -> void:
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(1.0 - value))	
-	#AudioServer.set_bus_volume_db(
-		#bus_index,
-		#linear_to_db(value)
-	#)	
+
+	# @Kawabaud: trans curr db linear 0-1, then scale slider range 0-100 & invert
+	value = MAX_VOL_DB * (1.0 - db_to_linear(AudioServer.get_bus_volume_db(bus_index)))
+
+func _volume_changer(slider_value: float) -> void:
+	# @Kawabaud: unit slider value->(0-1), inv it, & apply
+	var unit := 1.0 - (slider_value / MAX_VOL_DB)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(unit))
